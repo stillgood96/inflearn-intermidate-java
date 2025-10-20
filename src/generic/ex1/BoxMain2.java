d1dcf45a6405fe90e177319dbc1925f0ad453048
Sunwoo Lee <sunwool1@fpt.com>
2025-10-20 14:46:52 +0900

    first commit

---
+++ src/generic/ex1/BoxMain2.java
@@ -0,0 +1,18 @@
+package generic.ex1;
+
+public class BoxMain2 {
+
+  public static void main(String[] args) {
+
+    ObjectBox integerBox = new ObjectBox();
+    integerBox.setValue(10);
+
+    Integer integer = (Integer) integerBox.getValue();
+    System.out.println("integer = " + integer);
+
+    ObjectBox stringBox = new ObjectBox();
+    stringBox.setValue("string");
+    String string = (String) stringBox.getValue();
+    System.out.println("string = " + string);
+  }
+}
