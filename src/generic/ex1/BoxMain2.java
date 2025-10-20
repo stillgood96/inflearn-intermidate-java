0ff78759f15bf4fb49c042763a45ec54dc4a76b8
stillgood96 <a787574@naver.com>
2025-10-20 15:06:42 +0900

    6. 다형성을 통한 중복 해결 시도

--- src/generic/ex1/BoxMain2.java
+++ src/generic/ex1/BoxMain2.java
@@ -2,6 +2,16 @@ package generic.ex1;
 
 public class BoxMain2 {
 
+  /**
+   * ObjectBox의 경우
+   * Object는 코드의 중복을 줄여주지만 타입 안정성은 보장하지 못한다.
+   *
+   * IntegerBox, StringBox..etc 는 타입안정성은 높지만
+   * 코드의 중복이 늘어난다 (구현해야할 타입만큼)
+   * 이에 생기는 딜레마를 해결해주는 부분이 제네릭(Generic)이다
+   *
+   * @param args
+   */
   public static void main(String[] args) {
 
     ObjectBox integerBox = new ObjectBox();
