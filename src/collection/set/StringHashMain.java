c8008439adc913904bca3f1e7cfcdf42ba15b267
stillgood96 <a787574@naver.com>
2025-10-30 10:39:42 +0900

    문자열 해시 코드

---
+++ src/collection/set/StringHashMain.java
@@ -0,0 +1,38 @@
+package collection.set;
+
+public class StringHashMain {
+  static final int CAPACITY = 10;
+
+  public static void main(String[] args) {
+    //char
+    char charA = 'A';
+    char charB = 'B';
+    System.out.println("charA = " + (int)charA);
+    System.out.println("charB = " + (int)charB);
+
+    //hashCode
+    System.out.println("hashCode('A') = " + hashCode("A"));
+    System.out.println("hashCode('B') = " + hashCode("B"));
+    System.out.println("hashCode('C') = " + hashCode("C"));
+    System.out.println("hashCode('AB') = " + hashCode("AB"));
+
+    //hashInde
+    System.out.println("hashIndex('A') = " + hashIndex(hashCode("A")));
+    System.out.println("hashIndex('B') = " + hashIndex(hashCode("B")));
+    System.out.println("hashIndex('C') = " + hashIndex(hashCode("C")));
+    System.out.println("hashIndex('AB') = " + hashIndex(hashCode("AB")));
+  }
+
+  static int hashIndex(int value) {
+    return value % CAPACITY;
+  }
+
+  static int hashCode(String str) {
+    char[] charArray = str.toCharArray();
+    int sum = 0;
+    for (char c : charArray) {
+      sum += (int) c;
+    }
+    return sum;
+  }
+}
